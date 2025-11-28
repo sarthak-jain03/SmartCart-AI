94ac3af23e5065cf97471ed42d5b9d896e23a7f4
Sarthak Jain <sarthakjain4452@gmail.com>
2025-11-28 15:48:03 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
       <Link to="/">
-        <h2 className="text-2xl font-bold text-primary">Smart Grocery Store</h2>
+        <h2 className="text-2xl font-bold text-primary">SmartCart AI</h2>
       </Link>
 
       {/* Desktop Menu */}
